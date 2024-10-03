c5cb686082c808fac0d6832796871c4de79f749b
Christopher Young <christopheryoung@MacBook123.local>
2024-10-02 20:33:53 -0700

    implemented user login

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,10 +1,33 @@
 import "./LoginForm.css";
+// Bootstrap
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const Login = () => {
+const Login = ({ username, password, setUsername, setPassword, handleSubmit }) => {
   return (
-    <div>
-      Login form
-    </div>
+    <Form id = "login-form" onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Username</Form.Label>
+        <Form.Control 
+          onChange={e => setUsername(e.target.value)}
+          value={username}
+          type="text"
+          placeholder="Enter username" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control 
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+          type="password" 
+          placeholder="Password" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   )
 };
 
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,47 @@
 import "./Login.css";
+// React
+import { useState } from "react";
+// Routing
+import { useNavigate } from "react-router-dom";
+// Components
+import LoginForm from "../../components/forms/LoginForm";
+// APIs
+import AuthAPI from "../../apis/AuthAPI.ts";
 
 const Login = () => {
+  // Controlled input
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // Hooks
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+    AuthAPI.login(username, password)
+      .then(res => {
+        if(res.data.success) {
+          console.log(res.data.user);
+          navigate("/");
+        } else {
+          console.log(res.data.message);
+          setUsername("");
+          setPassword("");
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
     <div id="login">
-      Login
+      <h1>Login</h1>
+      <div>
+        <LoginForm
+          username={ username }
+          password={ password }
+          setUsername={ setUsername }
+          setPassword={ setPassword }
+          handleSubmit={ handleSubmit }/>
+      </div>
     </div>
   )
 };
