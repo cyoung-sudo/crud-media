eee62d2f254e79cbd60ffb17aea0d1c699a713c1
Christopher Young <christopheryoung@MacBook123.local>
2024-10-07 14:22:52 -0700

    added posts-list display

---
+++ src/components/lists/PostsList.tsx
@@ -0,0 +1,32 @@
+import "./UsersList";
+// Bootstrap
+import ListGroup from 'react-bootstrap/ListGroup';
+
+interface Post {
+  userId: string,
+  username: string,
+  title: string;
+  text: string;
+  createdAt: Date;
+}
+
+interface Props {
+  posts: Post[];
+};
+
+const PostsLists: React.FC<Props> = ({ posts }) => {
+  return (
+    <ListGroup>
+      {posts.map((post: Post, idx: number) => (
+        <ListGroup.Item key={idx}>
+          <div>{post.username}</div>
+          <div>{post.title}</div>
+          <div>{post.text}</div>
+          <div>{new Date(post.createdAt).toDateString()}</div>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
+
+export default PostsLists;
--- src/pages/posts/AllPosts.tsx
+++ src/pages/posts/AllPosts.tsx
@@ -1,11 +1,35 @@
 import "./AllPosts.css";
+// React
+import { useState, useEffect } from "react";
+// Components
+import PostsLists from "../../components/lists/PostsList";
+// APIs
+import PostAPI from "../../apis/PostAPI";
 
 const AllPosts = () => {
-  return (
-    <div id="allPosts">
-      AllPosts
-    </div>
-  )
+  // Retrieved data
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    PostAPI.getAll()
+      .then(res => {
+        if(res.data.success) {
+          setPosts(res.data.posts);
+        }
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  if(posts) {
+    return (
+      <div id="allPosts">
+        <h1>Posts</h1>
+        <div>
+          <PostsLists posts={posts}/>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default AllPosts;
