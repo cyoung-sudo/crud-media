0e073b2531f35c8e8553129773e5a8a6b0bfe592
Christopher Young <christopheryoung@MacBook123.local>
2024-10-14 19:48:23 -0700

    added navbar collapse

--- src/components/navigation/Navigationbar.tsx
+++ src/components/navigation/Navigationbar.tsx
@@ -3,22 +3,48 @@ import "./Navigationbar.css";
 import { NavLink } from "react-router-dom";
 // Hooks
 import { useAuth } from "../../hooks/AuthProvider";
+// Bootstrap
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Navigationbar = () => {
   // Hooks
   const auth = useAuth();
 
   return (
-    <div id="navigationbar">
-      <div id="navigationbar-logo"><NavLink to="/">CRUD MEDIA</NavLink></div>
-      <ul id="navigationbar-links">
-        <li><NavLink to="/users">Users</NavLink></li>
-        <li><NavLink to="/posts">Posts</NavLink></li>
-        {auth.authUser && <li><button onClick={auth.logout}>Logout</button></li>}
-        {!auth.authUser && <li><NavLink to="/auth/signup">Signup</NavLink></li>}
-        {!auth.authUser && <li><NavLink to="/auth/login">Login</NavLink></li>}
-      </ul>
-    </div>
+    <Navbar id="navigationbar" expand="lg">
+      <Navbar.Brand id="navigationbar-logo"><NavLink to="/">CRUD MEDIA</NavLink></Navbar.Brand>
+      <Navbar.Toggle id="navigationbar-toggle" aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav id="navigationbar-links">
+          <LinkContainer to="/users" activeClassName="active">
+            <Nav.Link>Users</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/posts">
+            <Nav.Link>Posts</Nav.Link>
+          </LinkContainer>
+          {!auth.authUser &&
+            <LinkContainer to="/auth/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+          }
+          {!auth.authUser &&
+            <LinkContainer to="/auth/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          }
+          {auth.authUser && 
+            <LinkContainer to={`/users/${auth.authUser._id}`}>
+              <Nav.Link>Profile</Nav.Link>
+            </LinkContainer>
+          }
+          {auth.authUser && 
+            <button onClick={auth.logout}>Logout</button>
+          }
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 };
 
