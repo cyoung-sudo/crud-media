4ca74cc358077cc73a0e2c7eeb0bbf5f187565ec
Christopher Young <christopheryoung@MacBook123.local>
2024-10-15 14:15:50 -0700

    updated homepage

--- src/pages/home/Homepage.tsx
+++ src/pages/home/Homepage.tsx
@@ -1,9 +1,36 @@
 import "./Homepage.css";
+// Routing
+import { useNavigate } from "react-router-dom";
+// Bootstrap
+import Button from 'react-bootstrap/Button';
+// Icons
+import { IoEnterOutline } from "react-icons/io5";
+import { TfiWrite } from "react-icons/tfi";
+import { FaUsers } from "react-icons/fa6";
 
 const Homepage = () => {
+  // Hooks
+  const navigate = useNavigate();
+
   return (
     <div id="homepage">
-      Homepage
+      <div>
+        <h1><span><IoEnterOutline/></span> Join</h1>
+        <p>Create an account & join the community</p>
+        <Button onClick={() => navigate("/auth/signup")}>Signup</Button>
+      </div>
+
+      <div>
+        <h1><span><TfiWrite/></span> Posts</h1>
+        <p>View all posts from our users</p>
+        <Button onClick={() => navigate("/posts")}>View Posts</Button>
+      </div>
+
+      <div>
+        <h1><span><FaUsers/></span> Users</h1>
+        <p>View all users in our community</p>
+        <Button onClick={() => navigate("/users")}>View Users</Button>
+      </div>
     </div>
   )
 };
