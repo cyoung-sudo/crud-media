884fc926d25e06e5203dbf4ce3370ea62c3f6617
Christopher Young <christopheryoung@MacBook123.local>
2024-10-17 11:03:00 -0700

    added pagination to users page

--- src/pages/users/AllUsers.tsx
+++ src/pages/users/AllUsers.tsx
@@ -1,8 +1,16 @@
 import "./AllUsers.css";
+// React
+import { useState, useEffect } from "react";
 // Routing
 import { useLoaderData } from "react-router-dom";
 // Components
 import UsersList from "../../components/lists/UsersList";
+// Hook
+import usePagination from "../../hooks/usePagination";
+// Bootstrap
+import Button from 'react-bootstrap/Button';
+// Icons
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
 interface User {
   _id: string;
@@ -14,12 +22,28 @@ interface User {
 const AllUsers = () => {
   // Loader data
   const users = useLoaderData() as User[];
+  // Page data
+  const [pageData, setPageData] = useState<User[] | null>(null);
+  // Hooks
+  const { currentPage, totalPages, currentData, nextPage, prevPage } = usePagination(users, 2);
+
+  useEffect(() => {
+    let pageUsers = currentData();
+    setPageData(pageUsers);
+  }, [currentPage])
 
   return (
     <div id="allUsers">
       <h1>Users</h1>
-      <div id="allUsers-list">
-        <UsersList users={users}/>
+      {pageData && 
+        <div id="allUsers-list">
+          <UsersList users={pageData}/>
+        </div>
+      }
+      <div id="allUsers-pagination">
+        <Button onClick={prevPage} variant="primary"><FaArrowLeftLong/></Button>
+        <span>{currentPage}/{totalPages}</span>
+        <Button onClick={nextPage} variant="primary"><FaArrowRightLong/></Button>
       </div>
     </div>
   );
