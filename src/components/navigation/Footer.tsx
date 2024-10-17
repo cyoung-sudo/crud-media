949b9a7877546d9affdd81e012a59cf77551c267
Christopher Young <christopheryoung@MacBook123.local>
2024-10-17 15:25:41 -0700

    updated meta data

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div id="footer">
-      Footer
+      <a href="https://www.flaticon.com/free-icons/crud" title="crud icons" target="_blank" rel="noreferrer">Crud icons created by orvipixel - Flaticon</a>
     </div>
   )
 };
